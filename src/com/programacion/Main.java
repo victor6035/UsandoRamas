87763cd8f9e14a3fd31622a4219faa9dd1358ed8
VictorSaaPerez <vsaaperez@danielcastelao.org>
2021-01-12 11:06:42 +0100

    Modificaciones en la principal

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Rama principal");
+        System.out.println("Hago modificaciones en la rama principal");
     }
 }

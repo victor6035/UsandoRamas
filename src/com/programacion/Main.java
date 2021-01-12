caa66789cf1565686d12efe72a19f16b41b5fb5f
VictorSaaPerez <vsaaperez@danielcastelao.org>
2021-01-12 11:29:16 +0100

    Desplazo lineas para evitar conflicto

--- src/com/programacion/Main.java
+++ src/com/programacion/Main.java
@@ -3,6 +3,8 @@ package com.programacion;
 public class Main {
 
     public static void main(String[] args) {
+
+
         System.out.println("Salgo de la Rama principal");
         System.out.println("Voy a la bugFix");
     }
